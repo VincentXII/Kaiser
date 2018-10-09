f74779a734b511c54b71578de32987d71e20ece2
Vincent <vinlark@outlook.com>
2018-10-09 18:40:55 -0500

    Added show current directory to GUI

--- gui.py
+++ gui.py
@@ -1,22 +1,35 @@
 import subprocess, sys, os, shutil, tkinter
 from tkinter import *
 
-kaiser = Tk()
+class Kaiser(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self,master)
+        self.pack()
 
-f= Frame(kaiser, height=150, width=360)
+root = Tk()
+
+kaiser = Kaiser(master=root)
+kaiser.master.title("Kaiser GUI v0.1")
+
+f= Frame(root, height=150, width=360)
 f.pack_propagate(0)
 f.pack()
 
-L = Label(kaiser, anchor=NE, text="Kaiser GUI v0.1")
+L = Label(root, anchor=NE, text="Kaiser GUI v0.1")
 L.pack()
 
 def exit():
     global root
-    kaiser.quit()
+    root.quit()
 
+def currentdir():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    print(os.getcwd())
 
+currentdir = Button(root, text="Get Current Directory", command=currentdir)
+currentdir.pack() 
 
-exit = Button(kaiser, text="Exit Kaiser", command=exit)
+exit = Button(root, text="Exit Kaiser", command=exit)
 exit.pack(fill=BOTH, expand=1)
 
 mainloop()
